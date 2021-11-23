0c52424fb7be0c2f72fd35915b71f21b9716235c
mohaddeseamel <mohaddese.computer@gmail.com>
2021-11-23 10:39:23 -0800

    create button component

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="app">
+       <base-button>custom button</base-button>
+       <base-button color="danger" is-light/>
+       <base-button color="primary" size="medium" rounded />
+       <base-button color="success" is-light outlined/>
+       <base-button color="info" size="small" is-full @click="handleLoading" :loading="isLoading"/>
+    </div>
+    
+</template>
+
+<script>
+import BaseButton from './components/BaseButton.vue'
+export default {
+    name:'App',
+    data(){
+        return{
+            isLoading:false
+        }
+    },
+   
+   components:{
+       BaseButton
+   },
+   methods: {
+       handleLoading(){
+           this.isLoading = true;
+           console.log('loading')
+       }
+   },
+}
+</script>
+
+<style lang="scss">
+    @import "../node_modules/bulma/sass/utilities/_all";
+    @import '../node_modules/bulma/sass/elements/button.sass';
+
+</style>
---
+++ src/components/BaseButton.vue
@@ -0,0 +1,55 @@
+<template>
+    <button class="button" 
+    :class="{[`is-${color}`] : color ,
+     'is-light': isLight ,
+      [`is-${size}`] :size ,
+       'is-fullwidth':isFull,
+       'is-outlined':outlined,
+       'is-rounded': rounded,
+       'is-loading': loading
+       }"
+       
+    >
+    <slot>default click</slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name:'BaseButton',
+    props:{
+        color:{
+            type:String,
+            required:false
+        },
+        isLight:{
+            type:Boolean,
+            default:false
+        },
+        size:{
+            type:String,
+            default:"normal"
+        },
+        isFull:{
+            type:Boolean,
+            default:false
+        },
+        outlined:{
+            type:Boolean,
+            default:false
+        },
+        rounded:{
+            type:Boolean,
+            default:false
+        },
+         loading:{
+            type:Boolean,
+            default:false
+        }
+    }
+}
+</script>
+
+<style>
+    
+</style>
